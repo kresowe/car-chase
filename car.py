1294c8b7ba4d4241d4220232416ae0060180a567
kresowe <michal.barej@yahoo.com>
2024-02-24 08:30:55 +0100

    Added explaining comments.

--- car.py
+++ car.py
@@ -1,8 +1,8 @@
 class Car(object):
     def __init__(self, x0, v0, a, marker_color, marker='s'):
-        self.x0 = x0
-        self.v0 = v0
-        self.a = a
+        self.x0 = x0  # initial position
+        self.v0 = v0  # initial speed
+        self.a = a  # constant acceleration
         self.marker = marker
         self.marker_color = marker_color
 
@@ -13,4 +13,6 @@ class Car(object):
         return self.marker_color
 
     def position(self, t):
+        """Position at time t.
+        Assume car is moving with uniformly accelerated motion (or constant speed)."""
         return self.x0 + self.v0 * t + 0.5 * self.a * t**2
--- draw_cars_basic.py
+++ draw_cars_basic.py
@@ -5,7 +5,7 @@ from drawing import draw_road, draw_car, drawing_settings
 if __name__ == '__main__':
     car1 = Car(x0=0, v0=0, a=1.5, marker_color='red')
     car2 = Car(x0=0, v0=11.11, a=0.8, marker_color='green')
-    
+
     drawing_settings(-10, 800)
     draw_road()
     draw_car(car1, t=5, y=0.45)
