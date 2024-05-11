a8be884110e6b6c2743e2e8505f8d508a4acf1f8
kresowe <michal.barej@yahoo.com>
2024-05-11 17:47:13 +0200

    Added type hinting in car and drawing

--- car.py
+++ car.py
@@ -1,23 +1,36 @@
-class Car(object):
-    def __init__(self, x0, v0, a, marker_color, marker='s'):
-        self._x0 = x0  # initial position
-        self._v0 = v0  # initial speed
-        self._a = a  # constant acceleration
+from matplotlib import axes, lines
+from numpy.typing import ArrayLike, NDArray
+
+
+class Car:
+    def __init__(self, x0: float, v0: float, a: float, marker_color: str, marker: str = 's') -> None:
+        """
+        :param x0: initial position [m]
+        :param v0: initial speed [m/s]
+        :param a: acceleration assumed to be constant [m/s**2]
+        :param marker_color: color of a marker (color in plt.plot())
+        :param marker: marker that shows a car's position (marker in plt.plot())
+        """
+        self._x0 = x0
+        self._v0 = v0
+        self._a = a
         self._marker_color = marker_color
         self._marker = marker
 
     @property
-    def marker(self):
+    def marker(self) -> str:
         return self._marker
 
     @property
-    def marker_color(self):
+    def marker_color(self) -> str:
         return self._marker_color
 
-    def position(self, t):
-        """Position at time t.
-        Assume car is moving with uniformly accelerated motion (or constant speed)."""
+    def position(self, t: ArrayLike) -> NDArray:
+        """If t is a single number, the function returns car's position at time t [s].
+        If t is a list or numpy array, the function returns car's position at each time in t [s].
+        It is assumed that a car is moving with uniformly accelerated motion (or constant speed)."""
         return self._x0 + self._v0 * t + 0.5 * self._a * t**2
 
-    def plot_dummy(self, ax):
+    def plot_dummy(self, ax: axes.Axes) -> list[lines.Line2D]:
+        """Makes a dummy plot of no points that will be updated by the cars markers during the animation."""
         return ax.plot([], [], color=self._marker_color, marker=self._marker, markersize=12)
--- drawing.py
+++ drawing.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from car import Car
+from matplotlib import axes, figure, text
+from typing import Optional
+from numpy.typing import ArrayLike
 
 
-def drawing_settings(x_min, x_max, ax=None, fig=None):
+def drawing_settings(x_min: float, x_max: float,
+                     ax: Optional[axes.Axes] = None,
+                     fig: Optional[figure.Figure] = None) -> None:
     plt.rcParams.update({'font.size': 20})
     if ax is None:
         plt.rcParams['figure.figsize'] = [10, 3]
@@ -19,7 +24,7 @@ def drawing_settings(x_min, x_max, ax=None, fig=None):
         fig.tight_layout()
 
 
-def draw_road(low_y=0.4, high_y=0.6, ax=None):
+def draw_road(low_y: float = 0.4, high_y: float = 0.6, ax: Optional[axes.Axes] = None) -> None:
     if ax is None:
         plt.axhline(low_y, linewidth=2, color='black')
         plt.axhline(high_y, linewidth=2, color='black')
@@ -28,11 +33,11 @@ def draw_road(low_y=0.4, high_y=0.6, ax=None):
         ax.axhline(high_y, linewidth=2, color='black')
 
 
-def draw_car(car: Car, t: float, y: float = 0.5):
+def draw_car(car: Car, t: float, y: float = 0.5) -> None:
     plt.plot(car.position(t), y, color=car.marker_color, marker=car.marker, markersize=12)
 
 
-def find_x_range(car1_positions, car2_positions):
+def find_x_range(car1_positions: ArrayLike, car2_positions: ArrayLike) -> tuple[float, float]:
     car1_min_position, car2_min_position = np.min(car1_positions), np.min(car2_positions)
     car1_max_position, car2_max_position = np.max(car1_positions), np.max(car2_positions)
     pos_min = min(car1_min_position, car2_min_position)
@@ -43,5 +48,5 @@ def find_x_range(car1_positions, car2_positions):
     return x_min, x_max
 
 
-def get_time_text(ax):
+def get_time_text(ax: axes.Axes) -> tuple[str, text.Text]:
     return 'time = {:.1f} s', ax.text(0.05, 0.85, '', transform=ax.transAxes)
